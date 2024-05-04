12d7f3e6a37a76fa6896714d8e8b1d535ec573b0
Devesh Gupta <deveshgupta231@gmail.com>
2024-05-04 17:51:14 +0530

    Navbar added in account page

--- client/src/pages/AccountPage.jsx
+++ client/src/pages/AccountPage.jsx
@@ -1,9 +1,20 @@
 import React, { useContext } from 'react';
 import UserContext from '../context/UserContext';
-import { Navigate } from 'react-router-dom';
+import { Link, Navigate, useParams } from 'react-router-dom';
 
 function AccountPage() {
   const { user, ready } = useContext(UserContext);
+  var { subpage } = useParams();
+  if (subpage === undefined) {
+    subpage = 'profile';
+  }
+
+  function linkClasses(type) {
+    var dynamciClassName = 'px-4 py-2';
+    if (type === subpage)
+      dynamciClassName += ' border rounded-full bg-primary text-white';
+    return dynamciClassName;
+  }
 
   if (!ready) {
     return <div>Loading...</div>;
@@ -11,7 +22,21 @@ function AccountPage() {
   if (ready && !user) {
     return <Navigate to={'/login'} />;
   }
-  return <div>AccountPage of {user?.Name}</div>;
+  return (
+    <div>
+      <nav className='w-full flex gap-5 justify-center mt-6'>
+        <Link className={linkClasses('profile')} to={'/account'}>
+          My Profile
+        </Link>
+        <Link className={linkClasses('bookings')} to={'/account/bookings'}>
+          My bookings
+        </Link>
+        <Link className={linkClasses('places')} to={'/account/places'}>
+          My accommodations
+        </Link>
+      </nav>
+    </div>
+  );
 }
 
 export default AccountPage;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,7 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: 'pink',
+        primary: '#FF9BD2',
       },
     },
   },
